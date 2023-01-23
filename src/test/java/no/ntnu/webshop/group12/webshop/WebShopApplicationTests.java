c4d492840cfd10ec979a92ed5b4edaa981956e9a
Dardnos <adne.o94@gmail.com>
2023-01-23 15:45:43 +0100

    Convert to Maven

---
+++ src/test/java/no/ntnu/webshop/group12/webshop/WebShopApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.webshop.group12.WebShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
