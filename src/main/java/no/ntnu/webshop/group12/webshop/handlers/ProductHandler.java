657622069c069ad65f424845010c574047b69224
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2023-03-13 17:07:05 +0100

    Some backbone of the web application

--- src/main/java/no/ntnu/webshop/group12/webshop/handlers/ProductHandler.java
+++
@@ -1,5 +0,0 @@
-package no.ntnu.webshop.group12.webshop.handlers;
-
-public class ProductHandler {
-
-}
---
+++ src/main/java/no/ntnu/webshop/group12/webshop/models/Category.java
@@ -0,0 +1,33 @@
+package no.ntnu.webshop.group12.webshop.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+
+    public Category() {
+    }
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
