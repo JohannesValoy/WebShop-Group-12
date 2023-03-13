cf57135ba6d8e748940a6e124503c433bf62ddbd
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2023-03-13 18:14:11 +0100

    Debugged and fixed

--- src/main/java/no/ntnu/webshop/group12/webshop/models/User.java
+++ src/main/java/no/ntnu/webshop/group12/webshop/models/User.java
@@ -1,11 +1,19 @@
 package no.ntnu.webshop.group12.webshop.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class User {
+    @Id
+    @GeneratedValue
+    private int id;
     private String username;
     private String password;
+
+    public User() {
+    }
 
     public User(String username, String password) {
         this.username = username;
