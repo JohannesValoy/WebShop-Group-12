7194d28edc8337f8806e7a7643c20aadcda59c45
Dardnos <adne.o94@gmail.com>
2023-03-13 16:20:36 +0100

    Added thymeleaf

--- src/main/java/no/ntnu/webshop/group12/webshop/WebShopApplication.java
+++ src/main/java/no/ntnu/webshop/group12/webshop/WebShopApplication.java
@@ -3,9 +3,11 @@ package no.ntnu.webshop.group12.webshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main entrypoint for the application.
+ */
 @SpringBootApplication
 public class WebShopApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebShopApplication.class, args);
 	}
