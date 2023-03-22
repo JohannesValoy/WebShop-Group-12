f30dabc12e1655b4947cd71da3a29cc632f8ed41
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2023-03-22 18:15:48 +0100

    Added some dummy html files and rest points

--- src/main/java/no/ntnu/webshop/group12/webshop/controllers/PageController.java
+++ src/main/java/no/ntnu/webshop/group12/webshop/controllers/PageController.java
@@ -4,9 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import no.ntnu.webshop.group12.webshop.models.dto.LoginDTO;
 import no.ntnu.webshop.group12.webshop.service.ProductService;
+import no.ntnu.webshop.group12.webshop.service.AccessUserService;
 
 /**
  * Controller for all HTML pages.
@@ -17,6 +21,9 @@ public class PageController {
     @Autowired
     ProductService productService;
 
+    @Autowired
+    AccessUserService userService;
+
     /**
      * The `Home` page.
      *
@@ -25,33 +32,92 @@ public class PageController {
     @GetMapping("/")
     public String getHome(Model model) {
         model.addAttribute("products", productService.getAllProducts());
+        model.addAttribute("user", userService.getSessionUser());
         return "index";
     }
 
+    /**
+     * The `Category` page.
+     * 
+     * @return Name of the ThymeLeaf template to render
+     */
     @GetMapping("/category")
-    public String getCategory() {
+    public String getCategory(Model model) {
+        model.addAttribute("user", userService.getSessionUser());
         return "category";
     }
 
+    /**
+     * The `Cart` page.
+     * 
+     * @return Name of the ThymeLeaf template to render
+     */
     @GetMapping("/cart")
-    public String getCart() {
+    public String getCart(Model model) {
+        model.addAttribute("user", userService.getSessionUser());
         return "cart";
     }
 
-    @GetMapping("/account")
-    public String getAccount() {
-        return "account";
+    /**
+     * The `Login` page.
+     * 
+     * @return Name of the ThymeLeaf template to render
+     */
+    @GetMapping("/login")
+    public String getLogin(Model model) {
+        model.addAttribute("user", userService.getSessionUser());
+        return "login";
     }
 
+    /**
+     * The `Register` page.
+     * 
+     * @return Name of the ThymeLeaf template to render
+     */
+    @GetMapping("/register")
+    public String getRegister(Model model) {
+        model.addAttribute("user", userService.getSessionUser());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String postRegister(@ModelAttribute LoginDTO signupDTO, Model model) {
+        model.addAttribute("signupDTO", signupDTO);
+        model.addAttribute("user", userService.getSessionUser());
+        String page = "login";
+        String error = userService.tryCreateNewUser(signupDTO.getUsername(), signupDTO.getPassword());
+        if (!error.isEmpty()) {
+            model.addAttribute("error", error);
+            page = "register";
+        }
+        return page;
+    }
+
+    /**
+     * The `About` page.
+     * 
+     * @return Name of the ThymeLeaf template to render
+     */
     @GetMapping("/about")
     public String getAbout() {
         return "about";
     }
 
+    /**
+     * The `Contact` page.
+     * 
+     * @return Name of the ThymeLeaf template to render
+     */
     @GetMapping("/products/{id}")
     public String getProduct(@PathVariable("id") int id, Model model) {
         model.addAttribute("product", productService.getProduct(id));
+        model.addAttribute("user", userService.getSessionUser());
         return "product";
     }
 
+    @GetMapping("/user/{id}")
+    public String getUser(@PathVariable("id") int id, Model model) {
+        model.addAttribute("user", userService.getSessionUser());
+        return "user";
+    }
 }
---
+++ src/main/java/no/ntnu/webshop/group12/webshop/service/AccessUserService.java
@@ -0,0 +1,75 @@
+package no.ntnu.webshop.group12.webshop.service;
+
+import java.util.regex.Pattern;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+
+import no.ntnu.webshop.group12.webshop.models.User;
+import no.ntnu.webshop.group12.webshop.repository.UserRepository;
+
+@Service
+public class AccessUserService {
+
+    static final Pattern usernamePattern = Pattern.compile("^([A-Za-z0-9]){2,}$");
+    static final Pattern passwordPattern = Pattern.compile("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$");
+
+    @Autowired
+    UserRepository userRepository;
+
+    /**
+     * Tries to create a new user, returns an error message if it fails
+     * 
+     * @param username Users username
+     * @param password Users password
+     * @return Error message or null if no error
+     */
+    public String tryCreateNewUser(String username, String password) {
+        String error = null;
+        if (userExist(username)) {
+            error = "User already exists";
+        }
+        if (error == null && !usernamePattern.matcher(username).matches()) {
+            error = "Invalid username, needs to be 2 or more characters and only contain letters and numbers";
+        }
+        if (error == null && !passwordPattern.matcher(password).matches()) {
+            error = "Invalid password needs to be 8 or more characters and contain at least one uppercase letter, one lowercase letter and one number";
+        }
+        if (error == null) {
+            User user = new User(username, createHash(password));
+            userRepository.save(user);
+        }
+        return error;
+    }
+
+    public User tryLogin(String username, String password) {
+        User user = userRepository.findByUsername(username).get();
+        if (user != null && checkPassword(password, user.getPassword())) {
+            return user;
+        }
+        return null;
+    }
+
+    public User getSessionUser() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+        String username = authentication.getName();
+        return userRepository.findByUsername(username).orElse(null);
+    }
+
+    private String createHash(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    private boolean checkPassword(String password, String hash) {
+        return BCrypt.checkpw(password, hash);
+    }
+
+    private boolean userExist(String username) {
+        return userRepository.findByUsername(username).isPresent();
+    }
+}
