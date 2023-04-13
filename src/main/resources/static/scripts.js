48b0467a8d82f504e9abb27ae4deff304dccc500
Dardnos <adne.o94@gmail.com>
2023-04-13 14:50:22 +0200

    Made confirm password red when not matched and at least 6 char long

--- src/main/resources/static/scripts.js
+++ src/main/resources/static/scripts.js
@@ -11,14 +11,32 @@ loginLink.onclick = () => {
     login.classList.remove("register");
 };
 
-function checkPasswordAlike(){
-    const password = document.getElementById("signUpPassword").value;
-    const password2 = document.getElementById("confirmSignUpPassword").value;
-    if(password != null && password != "" && password2 != null && password2 != ""){
-        if(password != password2){
-            document.getElementById("confirmSignUpPassword").style.backgroundColor = "red";
-        }else{
-            document.getElementById("confirmSignUpPassword").style.backgroundColor = "green";
-        }
+function checkPasswordAlike() {
+    const password = document.getElementById("signUpPassword");
+    const confirmPassword = document.getElementById("confirmSignUpPassword");
+    const message = document.getElementById("confirmation");
+    const submitButton = document.getElementById("submitButton");
+    const termsCheckbox = document.getElementById("termsCheckbox");
+
+    if (password.value !== confirmPassword.value) {
+        message.classList.add("password-mismatch");
+        message.innerHTML = "Passwords do not match";
+        submitButton.disabled = true;
+    } else if (password.value.length < 6){
+        message.classList.add("password-mismatch");
+        message.innerHTML = "Password must be at least 6 characters long";
+        submitButton.disabled = true;
+    } else {
+    message.classList.remove("password-mismatch");
+    message.innerHTML = "Confirm password";
+    submitButton.disabled = !termsCheckbox.checked;
     }
 }
+
+const passwordInput = document.getElementById("signUpPassword");
+const confirmPasswordInput = document.getElementById("confirmSignUpPassword");
+const termsCheckbox = document.getElementById("termsCheckbox");
+
+passwordInput.addEventListener("keyup", checkPasswordAlike);
+confirmPasswordInput.addEventListener("keyup", checkPasswordAlike);
+termsCheckbox.addEventListener("change", checkPasswordAlike);
