c0dc87e6c8365589514e9b3ab9ebe62a9ad2060e
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2023-04-13 14:28:24 +0200

    Made "Other products"

--- src/main/java/no/ntnu/webshop/group12/webshop/controllers/PageController.java
+++ src/main/java/no/ntnu/webshop/group12/webshop/controllers/PageController.java
@@ -108,6 +108,7 @@ public class PageController {
     public String getProduct(@PathVariable("id") int id, Model model) {
         model.addAttribute("product", productService.getProduct(id));
         model.addAttribute("user", userService.getSessionUser());
+        model.addAttribute("featuredProducts", productService.getRandomProductsByCategory(3, id));
         return "product";
     }
 
--- src/main/java/no/ntnu/webshop/group12/webshop/service/ProductService.java
+++ src/main/java/no/ntnu/webshop/group12/webshop/service/ProductService.java
@@ -1,8 +1,14 @@
 package no.ntnu.webshop.group12.webshop.service;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import no.ntnu.webshop.group12.webshop.models.product.Category;
 import no.ntnu.webshop.group12.webshop.models.product.Product;
 import no.ntnu.webshop.group12.webshop.repository.ProductRepository;
 
@@ -18,6 +24,39 @@ public class ProductService {
 
     public Product getProduct(int id) {
         return productRepository.findById(id);
+    }
+
+    public Set<Product> getRandomProducts(long i) {
+        Set<Product> products = new HashSet<>();
+        Random random = new Random();
+        if (i > productRepository.count()) {
+            i = productRepository.count();
+        }
+
+        for (int j = 0; i > j; j++) {
+            products.add(productRepository.findById((int) (random.nextInt() * productRepository.count())));
+        }
+        return products;
+    }
+
+    public Set<Product> getRandomProductsByCategory(long i, int id) {
+        Product baseProduct = productRepository.findById(id);
+        Set<Category> categories = baseProduct.getCategory();
+        return getRandomProductsByCategory(i, baseProduct.getCategory().toArray(new Category[categories.size()])[0]);
+    }
+
+    public Set<Product> getRandomProductsByCategory(long i, Category category) {
+        Set<Product> products = new HashSet<>();
+        Random random = new Random();
+        List<Product> productsByCategory = productRepository.findByCategory(category);
+        if (i > productsByCategory.size()) {
+            i = productsByCategory.size();
+        }
+
+        for (int j = 0; i > j; j++) {
+            products.add(productsByCategory.get((random.nextInt(productsByCategory.size()))));
+        }
+        return products;
     }
 
 }
