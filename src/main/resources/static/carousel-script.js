8a1427d7d305497dc45302a286d0a36cc713e5aa
JohannesValoy <johakv@stud.ntnu.no>
2023-04-13 14:29:12 +0200

    working on carousel

--- src/main/resources/static/carousel-script.js
+++ src/main/resources/static/carousel-script.js
@@ -1,22 +1,49 @@
-let slideIndex = 1;
+const carousel = document.querySelector('.carousel');
+const container = carousel.querySelector('.carousel-container');
+const items = carousel.querySelectorAll('.carousel-item');
+const prevBtn = carousel.querySelector('.carousel-prev');
+const nextBtn = carousel.querySelector('.carousel-next');
 
-function showSlide(n) {
-    let items = document.querySelectorAll(".carousel-item");
-    if (n > items.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = items.length;
-    }
-    items.forEach(item => item.style.transform = `translateX(-${(slideIndex-1) * 100}%)`);
+let currentIndex = 0;
+let interval;
+
+function startCarousel() {
+    interval = setInterval(() => {
+        currentIndex++;
+        if (currentIndex >= items.length) {
+            currentIndex = 0;
+        }
+        updateCarousel();
+    }, 3000);
+}
+
+function updateCarousel() {
+    container.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function stopCarousel() {
+    clearInterval(interval);
 }
 
 function prevSlide() {
-    showSlide(slideIndex -= 1);
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = items.length - 1;
+    }
+    updateCarousel();
 }
 
 function nextSlide() {
-    showSlide(slideIndex += 1);
+    currentIndex++;
+    if (currentIndex >= items.length) {
+        currentIndex = 0;
+    }
+    updateCarousel();
 }
 
-showSlide(slideIndex);
+prevBtn.addEventListener('click', prevSlide);
+nextBtn.addEventListener('click', nextSlide);
+
+startCarousel();
+carousel.addEventListener('mouseover', stopCarousel);
+carousel.addEventListener('mouseout', startCarousel);
