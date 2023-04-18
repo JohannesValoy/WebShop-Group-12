5cf64a01f1f35ca27027dea0ad118d830fd6d44c
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2023-04-18 04:33:56 +0200

    Adding now tests..... fun

--- src/test/java/no/ntnu/webshop/group12/webshop/controllers/CategoryControllerTest.java
+++ src/test/java/no/ntnu/webshop/group12/webshop/controllers/CategoryControllerTest.java
@@ -2,10 +2,21 @@ package no.ntnu.webshop.group12.webshop.controllers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import no.ntnu.webshop.group12.webshop.models.product.Category;
+
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 public class CategoryControllerTest {
 
     @Autowired
@@ -15,7 +26,14 @@ public class CategoryControllerTest {
     ObjectMapper objectMapper;
 
     @Test
-    void testCreateCategory() {
+    @WithMockUser(roles = "ADMIN")
+    void testCreateCategory() throws JsonProcessingException, Exception {
+
+        Category category = new Category("test");
+
+        mockMvc.perform(post("/api/category").content(objectMapper.writeValueAsString(category))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
 
     }
 
