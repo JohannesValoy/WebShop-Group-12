fc53713905a385036a83ed3c220f68bea2d9b86c
Dardnos <adne.o94@gmail.com>
2023-04-24 18:40:04 +0200

    work in progress

--- src/main/resources/static/js/cart-and-checkout.js
+++ src/main/resources/static/js/cart-and-checkout.js
@@ -5,16 +5,9 @@ const progressBubble2= document.querySelector(".progress-bubble2");
 const shippingSection = document.querySelector(".shipping-section");
 const progressBubble3 = document.querySelector(".progress-bubble3");
 const paymentSection = document.querySelector(".payment-section");
-const completeOrder= document.querySelector(".complete-order");
+const completeOrder= document.getElementById("completeOrder");
 const removeFromCartButton = document.querySelectorAll(".remove-from-cart-button");
 
-const nameField = document.querySelector(".name-field");
-const surNameField = document.querySelector(".surname-field");
-const addressField = document.querySelector(".address-field");
-const cardNumberField= document.querySelector(".card-number-field");
-const phoneNumberField = document.querySelector(".phone-number-field");
-const couponCodeField = document.querySelector(".card-coupon-field");
-const completeOrderbutton = document.querySelector(".complete-order-button");
 
 
 progressBubble1.addEventListener("click", () => {
@@ -65,6 +58,50 @@ progressBubble3.addEventListener("click", () => {
 progressBubble3.addEventListener("click", () => {
     shippingSection.classList.add("clicked");
 });
+
+function checkInfo() {
+    const username = document.getElementById("name");
+    const usernameText = document.getElementById("surName");
+    const password = document.getElementById("address");
+    const confirmPassword = document.getElementById("cardNumber");
+    const message = document.getElementById("phone");
+    const submitButton = document.getElementById("coupon");
+
+    const regexName = /^([A-Za-z0-9]){2,}$/;
+    const regexPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+
+    if (username.value === "") {
+        usernameText.classList.remove("password-mismatch");
+        usernameText.innerHTML = "Username";
+        submitButton.disabled = true;
+    } else if (!regexName.test(username.value)){
+        usernameText.classList.add("password-mismatch");
+        usernameText.innerHTML = "Invalid username, needs to be 2 or more characters and only contain letters and numbers";
+        submitButton.disabled = true;
+    } else {
+        usernameText.classList.remove("password-mismatch");
+        usernameText.innerHTML = "Username";
+        submitButton.disabled = !termsCheckbox.checked;
+    }
+
+    if (password.value === "" || confirmPassword.value === "") {
+        message.classList.remove("password-mismatch");
+        message.innerHTML = "Confirm password";
+        submitButton.disabled = true;
+    } else if (password.value !== confirmPassword.value) {
+        message.classList.add("password-mismatch");
+        message.innerHTML = "Passwords do not match";
+        submitButton.disabled = true;
+    } else if (!regexPass.test(password.value)){
+        message.classList.add("password-mismatch");
+        message.innerHTML = "Password must contain at least one uppercase letter, one lowercase letter, one number, and be at least 8 characters long";
+        submitButton.disabled = true;
+    } else {
+        message.classList.remove("password-mismatch");
+        message.innerHTML = "New password confirmed";
+        submitButton.disabled = !termsCheckbox.checked;
+    }
+}
 
 removeFromCartButton.forEach(button => {
     button.addEventListener("click", () => {
