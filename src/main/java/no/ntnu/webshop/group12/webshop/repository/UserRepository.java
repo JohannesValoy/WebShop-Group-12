52fbf861f647d8ac00406d9502653881f6af4f28
JeyKey09 <62012201+JeyKey09@users.noreply.github.com>
2023-04-26 18:35:28 +0200

    Forgot these depricating functions

--- src/main/java/no/ntnu/webshop/group12/webshop/repository/UserRepository.java
+++ src/main/java/no/ntnu/webshop/group12/webshop/repository/UserRepository.java
@@ -2,27 +2,10 @@ package no.ntnu.webshop.group12.webshop.repository;
 
 import java.util.Optional;
 
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
-import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.repository.CrudRepository;
-
-import com.querydsl.core.types.dsl.StringPath;
-
-import no.ntnu.webshop.group12.webshop.models.QUser;
 import no.ntnu.webshop.group12.webshop.models.User;
 
 public interface UserRepository
-        extends CrudRepository<User, Integer>, QuerydslPredicateExecutor<User>, QuerydslBinderCustomizer<QUser> {
+        extends CrudRepository<User, Integer> {
     Optional<User> findByUsername(String username);
-
-    @Override
-    default void customize(QuerydslBindings bindings, QUser root) {
-        bindings.bind(root.username).first((StringPath path, String value) -> path.like(value));
-        root.active.eq(true);
-        bindings.excluding(root.id);
-        bindings.excluding(root.roles);
-        bindings.excluding(root.active);
-        bindings.excluding(root.password);
-    }
 }
--- src/main/java/no/ntnu/webshop/group12/webshop/service/UserService.java
+++ src/main/java/no/ntnu/webshop/group12/webshop/service/UserService.java
@@ -33,9 +33,4 @@ public class UserService {
     public Optional<User> findUser(String name) {
         return userRepository.findByUsername(name);
     }
-
-    public List<User> getUsersByFilter(Predicate predicate, Pageable pageable) {
-        return userRepository.findAll(predicate, pageable).getContent();
-    }
-
 }
