da1117a2e270e8a847941a42932bd34be9a3c2a2
JeyKey09 <mathias-jk@live.no>
2023-05-07 10:51:28 +0200

    Removed filter from api link

--- src/main/java/no/ntnu/webshop/group12/webshop/controllers/CategoryController.java
+++ src/main/java/no/ntnu/webshop/group12/webshop/controllers/CategoryController.java
@@ -25,10 +25,8 @@ import com.querydsl.core.types.Predicate;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
 import no.ntnu.webshop.group12.webshop.excpetion.NotFoundException;
 import no.ntnu.webshop.group12.webshop.models.product.Category;
-import no.ntnu.webshop.group12.webshop.repository.CategoryRepository;
 import no.ntnu.webshop.group12.webshop.service.CategoryService;
 
 /**
@@ -64,7 +62,7 @@ public class CategoryController {
         return ResponseEntity.ok(categoryService.getCategoryCount());
     }
 
-    @GetMapping("/filter")
+    @GetMapping("")
     @Operation(summary = "Get categories by filter")
     public List<Category> getCategoriesByFilter(
             @ParameterObject @PageableDefault(size = 5, direction = Sort.Direction.ASC) Pageable pageable,
--- src/main/java/no/ntnu/webshop/group12/webshop/controllers/ProductController.java
+++ src/main/java/no/ntnu/webshop/group12/webshop/controllers/ProductController.java
@@ -3,7 +3,6 @@ package no.ntnu.webshop.group12.webshop.controllers;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -53,7 +52,7 @@ public class ProductController {
         return productService.getProductCount();
     }
 
-    @GetMapping("/filter")
+    @GetMapping("")
     @Operation(summary = "Get products by filter")
     public List<Product> getProductsByFilter(
             @ParameterObject @PageableDefault(size = 5, direction = Sort.Direction.ASC) Pageable pageable,
