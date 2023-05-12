29d780b91d78a094b26eeb9846255d4209c53ad0
JeyKey09 <mathias-jk@live.no>
2023-05-12 14:16:28 +0200

    Changing repositories

--- src/main/java/no/ntnu/webshop/group12/webshop/repository/purchase/PurchaseRepository.java
+++ src/main/java/no/ntnu/webshop/group12/webshop/repository/purchase/PurchaseRepository.java
@@ -2,13 +2,23 @@ package no.ntnu.webshop.group12.webshop.repository.purchase;
 
 import java.util.List;
 
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.repository.CrudRepository;
 
 import no.ntnu.webshop.group12.webshop.models.User;
 import no.ntnu.webshop.group12.webshop.models.purchase.Purchase;
+import no.ntnu.webshop.group12.webshop.models.purchase.QPurchase;
 
-public interface PurchaseRepository extends CrudRepository<Purchase, Integer> {
+public interface PurchaseRepository extends CrudRepository<Purchase, Integer>, QuerydslPredicateExecutor<Purchase>,
+QuerydslBinderCustomizer<QPurchase> {
 
     List<Purchase> findByUser(User sessionUser);
-    
+
+    @Override
+    default void customize(QuerydslBindings bindings, QPurchase root) {
+        bindings.excluding(root.products);
+
+    }
 }
--- src/main/java/no/ntnu/webshop/group12/webshop/repository/purchase/PurchasedItemRespository.java
+++ src/main/java/no/ntnu/webshop/group12/webshop/repository/purchase/PurchasedItemRespository.java
@@ -4,7 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import no.ntnu.webshop.group12.webshop.models.purchase.Item;
 
-//MEMO: Needs to be created for every purchase
 public interface PurchasedItemRespository extends CrudRepository<Item, Integer>{
     
 }
