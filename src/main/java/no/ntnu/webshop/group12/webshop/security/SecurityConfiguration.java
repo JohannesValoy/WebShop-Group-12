917bf601ba815f4815eb778cf5bff11b50a926bd
JeyKey09 <mathias-jk@live.no>
2023-05-18 15:29:19 +0200

    Migrated to DSL

--- src/main/java/no/ntnu/webshop/group12/webshop/security/SecurityConfiguration.java
+++ src/main/java/no/ntnu/webshop/group12/webshop/security/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 /**
  * This class is used to configure the security of the application
  */
@@ -30,41 +31,40 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain configureAuthorizationFilterChain(HttpSecurity http) throws Exception {
-        // Set up the authorization requests, starting from most restrictive at the top,
-        // to least restrictive on bottom
-        http.csrf().disable()
-                .authorizeHttpRequests()
+        //TODO: Clean up this mess
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                            .requestMatchers("/api/carts/**").hasAnyRole("USER", ADMIN)
+                            .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+                            .requestMatchers(HttpMethod.DELETE, "/api/**").hasRole(ADMIN)
+                            .requestMatchers(HttpMethod.POST, "/api/**").hasRole(ADMIN)
+                            .requestMatchers(HttpMethod.PUT, "/api/**").hasRole(ADMIN)
+                            .requestMatchers(HttpMethod.GET, "/api/purchases").hasRole(ADMIN)
+                            .requestMatchers(HttpMethod.GET, "/api/purchases/**").hasRole("USER")
+                            .requestMatchers(HttpMethod.GET, "/api/user").hasRole(ADMIN)
+                            .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+
+                            .requestMatchers("/account").hasAnyRole("USER", ADMIN)
+                            .requestMatchers("/cart", "/cart/**").hasAnyRole("USER", ADMIN)
+
+                            // Website endpoints
+                            .requestMatchers("/js/**").permitAll()
+                            .requestMatchers("/css/**").permitAll()
+                            .requestMatchers("/images/**").permitAll()
+                            .requestMatchers("/products/**").permitAll()
+                            .requestMatchers("/categories/**").permitAll()
+                            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                            .requestMatchers("/register").permitAll()
+                            .requestMatchers("/", "/about", "/search", "/error", "robots.txt").permitAll()
+                ).formLogin(formLogin -> formLogin.loginPage("/login").permitAll()).logout(logout -> logout.logoutSuccessUrl("/"));
 
                 // Admin endpoints
-                .requestMatchers("/admin/**").hasRole(ADMIN)
-
+                
                 // API endpoints
-                .requestMatchers("/api/carts/**").hasAnyRole("USER", ADMIN)
-                .requestMatchers(HttpMethod.POST, "/api/user").permitAll()
-                .requestMatchers(HttpMethod.DELETE, "/api/**").hasRole(ADMIN)
-                .requestMatchers(HttpMethod.POST, "/api/**").hasRole(ADMIN)
-                .requestMatchers(HttpMethod.PUT, "/api/**").hasRole(ADMIN)
-                .requestMatchers(HttpMethod.GET, "/api/purchases").hasRole(ADMIN)
-                .requestMatchers(HttpMethod.GET, "/api/purchases/**").hasRole("USER")
-                .requestMatchers(HttpMethod.GET, "/api/user").hasRole(ADMIN)
-                .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
-
+                
 
                 // Account endpoints
-                .requestMatchers("/account").hasAnyRole("USER", ADMIN)
-                .requestMatchers("/cart", "/cart/**").hasAnyRole("USER", ADMIN)
-
-                // Website endpoints
-                .requestMatchers("/js/**").permitAll()
-                .requestMatchers("/css/**").permitAll()
-                .requestMatchers("/images/**").permitAll()
-                .requestMatchers("/products/**").permitAll()
-                .requestMatchers("/categories/**").permitAll()
-                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                .requestMatchers("/register").permitAll()
-                .requestMatchers("/", "/about", "/search","/error", "robots.txt").permitAll()
-                .and().formLogin().loginPage("/login").permitAll()
-                .and().logout().logoutSuccessUrl("/");
+               
         return http.build();
     }
 
