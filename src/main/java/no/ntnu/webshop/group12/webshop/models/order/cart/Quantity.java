7703babda426cbf5f1745cd9116548d99f4782ee
JeyKey09 <mathias-jk@live.no>
2023-05-18 15:30:51 +0200

    Using jsonX instead of jsonignore

--- src/main/java/no/ntnu/webshop/group12/webshop/models/order/cart/Quantity.java
+++ src/main/java/no/ntnu/webshop/group12/webshop/models/order/cart/Quantity.java
@@ -1,7 +1,10 @@
 package no.ntnu.webshop.group12.webshop.models.order.cart;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import no.ntnu.webshop.group12.webshop.models.order.QuantityBase;
 import no.ntnu.webshop.group12.webshop.models.product.Product;
@@ -10,6 +13,10 @@ import no.ntnu.webshop.group12.webshop.models.product.Product;
 @Table(name = "quantities")
 @Schema(description = "A item within cart", name = "Quantity")
 public class Quantity extends QuantityBase{
+
+    @ManyToOne
+    @JsonBackReference
+    private Cart cart;
 
     public Quantity() {
         super();
