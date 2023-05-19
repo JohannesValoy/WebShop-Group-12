6fbe0626d56e70f868d28fa65c3eeeb901e0a892
JeyKey09 <mathias-jk@live.no>
2023-05-19 16:02:28 +0200

    Can now confirm cart with API

--- src/main/java/no/ntnu/webshop/group12/webshop/controllers/CartController.java
+++ src/main/java/no/ntnu/webshop/group12/webshop/controllers/CartController.java
@@ -8,16 +8,21 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.querydsl.core.types.Predicate;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import no.ntnu.webshop.group12.webshop.service.CartService;
+import no.ntnu.webshop.group12.webshop.models.dto.CartPurchase;
 import no.ntnu.webshop.group12.webshop.models.order.cart.Cart;
 import no.ntnu.webshop.group12.webshop.models.order.cart.Quantity;
+import no.ntnu.webshop.group12.webshop.models.order.purchase.Purchase;
 import no.ntnu.webshop.group12.webshop.models.product.Product;
 
 @RestController
@@ -58,5 +63,9 @@ public class CartController {
         return cartService.getCarts(predicate, pageable);
     }
 
-
+    @PostMapping("/confirm")
+    @Operation(summary = "Confirm cart")
+    public Purchase confirmCart(@RequestBody @Valid CartPurchase cartPurchase) {
+        return cartService.confirmCart(cartPurchase);
+    }
 }
