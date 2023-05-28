19e0bd298368a481dd863bb4d3c238db6d5ce45e
JeyKey09 <mathias-jk@live.no>
2023-05-28 18:24:10 +0200

    Removed unused import

--- src/main/java/no/ntnu/webshop/group12/webshop/controllers/api/CategoryController.java
+++ src/main/java/no/ntnu/webshop/group12/webshop/controllers/api/CategoryController.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
